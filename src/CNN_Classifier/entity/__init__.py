37bfcc02161ef10952593ada7cd24af83d03d8a8
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-12 17:38:17 +0530

    deep import

--- src/CNN_Classifier/entity/__init__.py
+++ src/CNN_Classifier/entity/__init__.py
@@ -1,1 +1,7 @@
-from CNN_Classifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from CNN_Classifier.entity.config_entity import (
+    DataIngestionConfig,
+    PrepareBaseModelConfig,
+    PrepareCallbacksConfig,
+    TrainingConfig,
+    EvaluationConfig,
+)
